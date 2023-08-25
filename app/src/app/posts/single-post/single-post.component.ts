08a9fc1049ceaa9766b2249aca3a00ed5c7d70ce
Marley <marley.nodejs@gmail.com>
2023-08-25 13:51:40 +0300

    58. Show the Single Post Details using Ngrx Data

--- app/src/app/posts/single-post/single-post.component.ts
+++ app/src/app/posts/single-post/single-post.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from 'src/app/models/post.model';
+import { PostService } from 'src/app/posts/post.service';
 
 @Component({
   selector: 'app-single-post',
   templateUrl: './single-post.component.html',
-  styleUrls: ['./single-post.component.css']
+  styleUrls: ['./single-post.component.css'],
 })
-export class SinglePostComponent {
+export class SinglePostComponent implements OnInit {
+  post?: Post;
 
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.params['id'];
+
+    this.postService.entities$.subscribe((posts) => {
+      this.post = posts.find((post) => post.id === id);
+    });
+  }
 }
